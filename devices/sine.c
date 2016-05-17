4ccfc257523b70f45d4d9053a037977ad09d2162
Joseph Marlin <j.marlin@outlook.com>
2016-05-17 14:59:38 +0000

    Added a cv-controlled sine vco signal generator. Now I just need to implement cv signals and a sequencer-to-cv adapter and I can start writing notes

--- devices/sine.c
+++ devices/sine.c
@@ -1,5 +1,6 @@
 #include "../channel/channel.h"
 #include "../config.h"
+#include "controlvoltage.h"
 #include <math.h>
 #include <stdlib.h>
 
@@ -9,16 +10,16 @@
 
 #define DEG_TO_RAD(x) (((x)*M_PI)/180.0)
 
-typedef struct SineGeneratorEnv {
+typedef struct FixedSineGeneratorEnv {
     float current_phase; //Position along the t axis in degrees
     float freq;          //Frequency of the generator
     float duration;      //How long to generate the signal in ms
     float time;           //How long the signal has been running in ms
-} SineGeneratorEnv;
+} FixedSineGeneratorEnv;
 
-int sine_generator(float* sample, void* environment) {
+int fixed_sine_generator(float* sample, void* environment) {
 
-    SineGeneratorEnv* vars = (SineGeneratorEnv*)environment;
+    FixedSineGeneratorEnv* vars = (FixedSineGeneratorEnv*)environment;
 
     float deg_per_sample = (360.0 * vars->freq)/SAMPLE_RATE;
 
@@ -37,14 +38,14 @@ int sine_generator(float* sample, void* environment) {
     return 1;
 }
 
-SignalSourceMono_f* new_sine_test(float freq, float duration) {
+SignalSourceMono_f* new_fixed_sine(float freq, float duration) {
 
-    SignalSourceMono_f* signal = new_ssmf(sine_generator);
+    SignalSourceMono_f* signal = new_ssmf(fixed_sine_generator);
     
     if(!signal)
         return signal;
 
-    SineGeneratorEnv* environment = (SineGeneratorEnv*)malloc(sizeof(SineGeneratorEnv));
+    FixedSineGeneratorEnv* environment = (FixedSineGeneratorEnv*)malloc(sizeof(SineGeneratorEnv));
 
     if(!environment) {
     
@@ -61,3 +62,62 @@ SignalSourceMono_f* new_sine_test(float freq, float duration) {
 
     return signal;
 }
+
+typedef struct SineVcoGeneratorEnv {
+    float current_phase;
+    unsigned last_state;
+    ControlVoltage* control_voltage;
+} SineVcoGeneratorEnv;
+
+int sine_vco_generator(float* sample, void* environment) {
+    
+    SineVcoGeneratorEnv* vars = (SineVcoGeneratorEnv*)environment;
+    float cv_pitch_sample, cv_gate_sample;
+
+    //If the control signal drops, we drop
+    if(!cv_pull_next_sample(&cv_pitch_sample, &cv_gate_sample, vars->control_voltage)) {
+        
+        sample[0] = 0;
+        return 0;
+    }
+    
+    //Control voltages assume a volt-to-hertz scheme covering eight
+    //octaves from A0 to A8  mapped over -1.0 to 1.0
+    //(eg: -1.0 = 55hz, 0.0 = 880hz, 1.0 = 14080hz)
+    float freq = 55.0 * pow(2, 8.0*(1.0 + cv_pitch_sample)/2.0);
+    float deg_per_sample = (360.0 * freq)/SAMPLE_RATE;
+
+    sample[0] = sinf(DEG_TO_RAD(vars->current_phase)); 
+
+    vars->current_phase += deg_per_sample;
+
+    if(vars->current_phase >= 360.0)
+        vars->current_phase -= 360.0;
+
+    return 1;
+}
+
+SignalSourceMono_f* new_sine_vco(ControlVoltage* control_voltage) {
+
+    SignalSourceMono_f* signal = new_ssmf(sine_vco_generator);
+
+    if(!signal)
+        return signal;
+
+    SineVcoGeneratorEnv* environment = (SineVcoGeneratorEnv*)malloc(sizeof(SineVcoGeneratorEnv));
+
+    if(!environment) {
+
+        //delete_ssmf(signal);
+        return (SignalSourceMono_f*)0;
+    }
+
+    environment->current_phase = 0;
+    environment->last_state = 0;
+    environment->control_voltage = control_voltage;
+    
+    signal->environment = environment;
+
+    return signal;
+}
+
--- devices/sine.h
+++ devices/sine.h
@@ -2,7 +2,9 @@
 #define SINE_H
 
 #include "../channel/channel.h"
+#include "controlvoltage.h"
 
-SignalSourceMono_f* new_sine_test(float freq, float duration);
+SignalSourceMono_f* new_fixed_sine(float freq, float duration);
+SignalSourceMono_f* new_sine_vco(ControlVoltage* control_voltage);
 
 #endif //SINE_H
