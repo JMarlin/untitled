233e74bcab8dcfce9ce7053f834cfe8f615254b0
Joseph Marlin <j.marlin@outlook.com>
2016-05-24 15:45:22 -0400

    Create sofilter.c

---
+++ devices/sofilter.c
@@ -0,0 +1,120 @@
+#include "../channel/channel.h"
+#include "../config.h"
+#include <stdlib.h>
+
+typedef struct SOFilterGeneratorEnv {
+    float b0;
+    float b1;
+    float b2;
+} SOFilterGeneratorEnv;
+
+int so_filter_generator(float* sample, void* environment) {
+
+    SOFilterGeneratorEnv* vars = (SOFilterGeneratorEnv*)environment;
+
+    sample[0] = vars->wave_function(vars->current_phase, vars->wave_environment);
+
+    return 1;
+}
+
+SignalSourceMono_f* new_fixed_wave(float freq, float duration, WaveFunction wave_function, void* wave_environment) {
+
+    SignalSourceMono_f* signal = new_ssmf(fixed_wave_generator);
+    
+    if(!signal)
+        return signal;
+
+    FixedWaveGeneratorEnv* environment = (FixedWaveGeneratorEnv*)malloc(sizeof(FixedWaveGeneratorEnv));
+
+    if(!environment) {
+    
+        //delete_ssmf(signal);
+        return (SignalSourceMono_f*)0;
+    }
+
+    environment->current_phase = 90.0;
+    environment->freq = freq;
+    environment->duration = duration;
+    environment->wave_function = wave_function;
+    environment->wave_environment = wave_environment;
+    environment->time = 0;
+
+    signal->environment = environment;
+
+    return signal;
+}
+
+typedef struct VcoWaveGeneratorEnv {
+    float current_phase;
+    unsigned last_state;
+    WaveFunction wave_function;
+    void* wave_environment;
+    ControlVoltage* control_voltage;
+} VcoWaveGeneratorEnv;
+
+int vco_wave_generator(float* sample, void* environment) {
+    
+    VcoWaveGeneratorEnv* vars = (VcoWaveGeneratorEnv*)environment;
+    float cv_pitch_sample, cv_gate_sample;
+
+    //If the control signal drops, we drop
+    if(!cv_pull_next_sample(vars->control_voltage, &cv_pitch_sample, &cv_gate_sample)) {
+        
+        sample[0] = 0;
+        return 0;
+    }
+    
+    //Control voltages assume a volt-to-hertz scheme covering eight
+    //octaves from A0 to A8  mapped over -1.0 to 1.0
+    //(eg: -1.0 = 55hz, 0.0 = 880hz, 1.0 = 14080hz)
+    if(cv_gate_sample > -1.0) { 
+
+        if(!vars->last_state) {
+
+            vars->current_phase = 90.0;
+            vars->last_state = 1;
+        }
+
+        float freq = 55.0 * pow(2, 8.0*(1.0 + cv_pitch_sample)/2.0);
+        float deg_per_sample = (360.0 * freq)/SAMPLE_RATE;
+
+        sample[0] = vars->wave_function(vars->current_phase, vars->wave_environment); 
+
+        vars->current_phase += deg_per_sample;
+
+        if(vars->current_phase >= 360.0)
+            vars->current_phase -= 360.0;
+    } else {
+   
+        sample[0] = 0;
+        vars->last_state = 0;
+    }
+
+    return 1;
+}
+
+SignalSourceMono_f* new_vco_wave(ControlVoltage* control_voltage, WaveFunction wave_function, void* wave_environment) {
+
+    SignalSourceMono_f* signal = new_ssmf(vco_wave_generator);
+
+    if(!signal)
+        return signal;
+
+    VcoWaveGeneratorEnv* environment = (VcoWaveGeneratorEnv*)malloc(sizeof(VcoWaveGeneratorEnv));
+
+    if(!environment) {
+
+        //delete_ssmf(signal);
+        return (SignalSourceMono_f*)0;
+    }
+
+    environment->current_phase = 90.0;
+    environment->last_state = 0;
+    environment->wave_function = wave_function;
+    environment->wave_environment = wave_environment;
+    environment->control_voltage = control_voltage;
+    
+    signal->environment = environment;
+
+    return signal;
+}
